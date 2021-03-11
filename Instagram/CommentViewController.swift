fe0ca47161d83ef33a1ce2e4c609825140debe9a
sokz05 <main@sokz.net>
2021-03-11 20:44:07 +0900

    Fix: Cellの制約でButtomを指定

---
+++ Instagram/CommentViewController.swift
@@ -0,0 +1,62 @@
+//
+//  CommentViewController.swift
+//  Instagram
+//
+//  Created by 井口創太 on 2021/03/11.
+//
+
+import UIKit
+import Firebase
+import SVProgressHUD
+
+class CommentViewController: UITabBarController, UITextFieldDelegate {
+
+  @IBOutlet weak var commentField: UITextField!
+  @IBOutlet weak var handleCommentButton: UIButton!
+  
+  var postData:PostData!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    commentField.text = ""
+    commentField.delegate = self
+    handleCommentButton.isEnabled = false
+  }
+  
+  @IBAction func checkComment(_ sender: Any) {
+    if commentField.text == "" {
+      handleCommentButton.isEnabled = false
+    } else {
+      handleCommentButton.isEnabled = true
+    }
+  }
+  
+  @IBAction func handleCommentButton(_ sender: Any) {
+    // HUDで処理中を表示
+    SVProgressHUD.show()
+    
+    //定義
+    let comment = commentField.text
+    let author = Auth.auth().currentUser?.displayName
+    let commentAuthor = "\(author!): \(comment!)"
+    var updateComment: FieldValue
+    
+    updateComment = FieldValue.arrayUnion([commentAuthor])
+    
+    //書き込み
+    let postRef = Firestore.firestore().collection(Const.PostPath).document(postData.id)
+    postRef.updateData(["commentAuthor": updateComment])
+    
+    SVProgressHUD.showSuccess(withStatus: "コメントを投稿しました！")
+    
+    //画面遷移
+    UIApplication.shared.windows.first{$0.isKeyWindow}?.rootViewController?.dismiss(animated: true, completion: nil)
+  }
+  
+  //キャンセルボタン
+  @IBAction func handleCancelButton(_ sender: Any) {
+    UIApplication.shared.windows.first{$0.isKeyWindow}?.rootViewController?.dismiss(animated: true, completion: nil)
+  }
+  
+}
---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 井口創太 on 2021/03/04.
+//
+
+import Foundation
+
+struct Const {
+  static let ImagePath = "images"
+  static let PostPath = "posts"
+}
